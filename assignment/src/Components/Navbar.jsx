6b7030cf246e7f906e153132e34f6cd6180e9ded
KartikAKGEC89 <s11kartik2111087@gmail.com>
2024-05-21 17:43:17 +0530

    FEAT: Finish header part

--- assignment/src/Components/Navbar.jsx
+++ assignment/src/Components/Navbar.jsx
@@ -1,24 +1,28 @@
 import React from 'react'
+import "./Navbar.css"
 
 const Navbar = () => {
   return (
     <>
-      <header className="header">
-        <nav className="navbar">
-          <div className="navbar-logo">LOGO</div>
-          <div className="navbar-buttons">
-            <button className="login-btn">Login</button>
-            <button className="signup-btn">Signup</button>
-          </div>
-        </nav>
-      </header>
-      <div className="nav-links-container">
-        <div className="nav-links-content">
-          <a href="#">Overview</a>
-          <a href="#">Curriculum</a>
-          <a href="#">Refund</a>
-          <a href="#">Testimonials</a>
+      <div class="flex flex-row pd-2 text-2xl m-5 header">
+        <div class="mr-6">Logo</div>
+        <div class="flex flex-row absolute top-4 right-6 text-center gap-6">
+          
+        <button class="text-pink-700 border-style: solid rounded-md p-2">
+          Login
+        </button>
+        
+        <button class="bg-pink-700 text-white border-style: solid rounded-md p-2">
+          Signup
+        </button>
+          
         </div>
+      </div>
+      <div class="bg-black flex flex-grow text-white text-center text-xl justify-center navbar">
+          <button class="px-2 py-2 hover:underline custom-underline">OverView</button>
+          <button class="px-2 py-2 hover:underline custom-underline">Curriculum</button>
+          <button class="px-2 py-2 hover:underline custom-underline">Refund</button>
+          <button class="px-2 py-2 hover:underline custom-underline">Testimonials</button>
       </div>
     </>
   )
